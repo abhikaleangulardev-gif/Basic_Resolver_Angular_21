759a49583ae854000ddf927f284b2ef5157f5a3e
abhishek.kale <abhisahebpatil@gmail.com>
2026-02-15 13:26:03 +0530

    completed development  header portion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '', pathMatch: 'full' },
+    { path: 'movie', loadComponent: () => import('./component/movie/movie').then((c) => c.Movie) },
+    { path: 'users', loadComponent: () => import('./component/users/users').then((c) => c.Users) },
+    { path: 'comments', loadComponent: () => import('./component/comments/comments').then((c) => c.Comments) },
+    { path: 'blog', loadComponent: () => import('./component/blog/blog').then((c) => c.Blog) },
+];
