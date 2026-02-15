2271883f2f3a85e9f1f06d6724f7ed67089239d0
abhishek.kale <abhisahebpatil@gmail.com>
2026-02-15 13:49:10 +0530

    completed implements routes files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,37 @@
 import { Routes } from '@angular/router';
+import { moviesResolver } from './resolver/movies-resolver';
+import { usersResolver } from './resolver/users-resolver';
+import { commentsResolver } from './resolver/comments-resolver';
+import { blogsResolver } from './resolver/blogs-resolver';
 
 export const routes: Routes = [
     { path: '', redirectTo: '', pathMatch: 'full' },
-    { path: 'movie', loadComponent: () => import('./component/movie/movie').then((c) => c.Movie) },
-    { path: 'users', loadComponent: () => import('./component/users/users').then((c) => c.Users) },
-    { path: 'comments', loadComponent: () => import('./component/comments/comments').then((c) => c.Comments) },
-    { path: 'blog', loadComponent: () => import('./component/blog/blog').then((c) => c.Blog) },
+    {
+        path: 'movie',
+        loadComponent: () => import('./component/movie/movie').then((c) => c.Movie),
+        resolve: {
+            movieList: moviesResolver,
+        }
+    },
+    {
+        path: 'users',
+        loadComponent: () => import('./component/users/users').then((c) => c.Users),
+        resolve: {
+            usersList: usersResolver,
+        }
+    },
+    {
+        path: 'comments',
+        loadComponent: () => import('./component/comments/comments').then((c) => c.Comments),
+        resolve: {
+            commentsList: commentsResolver,
+        }
+    },
+    {
+        path: 'blog',
+        loadComponent: () => import('./component/blog/blog').then((c) => c.Blog),
+        resolve: {
+            blogList: blogsResolver,
+        }
+    },
 ];
